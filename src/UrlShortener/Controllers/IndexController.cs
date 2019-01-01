ad1a9cff0e358a37651d44e62d6638e9b8ce5cfb
Luke Davies <lkdvs3@gmail.com>
2019-01-01 22:29:36 +0000

    Added base views

---
+++ src/UrlShortener/Controllers/IndexController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortener.Controllers
+{
+    [Route("/")]
+    public class IndexController : Controller
+    {
+        // GET: /
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /_admin
+        [HttpGet("_admin")]
+        public IActionResult Admin()
+        {
+            return View("Admin");
+        }
+
+        // GET: /{forward}
+        [HttpGet("{forward}")]
+        public string Forward(string forward)
+        {
+            return forward;
+        }
+    }
+}
